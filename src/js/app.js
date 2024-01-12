be3c16dd7bb3b2115960576de928ba41464cedf8
George Asiedu <georgeasiedu543@gmail.com>
2024-01-12 22:53:44 +0000

    Make range slider value in sync with pageview element

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,47 @@
 const toggler = document.querySelector(".toggle");
 const toggleIndicator = document.querySelector(".indicator");
+const viewEl = document.querySelector(".price-view");
+
+const mainPrice = document.querySelector(".main-price");
+
+const rangeInputEl = document.getElementById("range");
 
 toggler.addEventListener("click", () => {
   toggleIndicator.classList.toggle("clicked");
+});
+
+rangeInputEl.addEventListener("input", (e) => {
+  let prefix;
+  let rangeValue = Number(e.target.value);
+  let monthlyPrice = 0;
+
+  switch (rangeValue) {
+    case 10:
+      monthlyPrice = 8;
+      break;
+    case 50:
+      monthlyPrice = 12;
+      break;
+    case 100:
+      monthlyPrice = 16;
+      break;
+    case 500:
+      monthlyPrice = 24;
+      break;
+    case 1000:
+      monthlyPrice = 36;
+      break;
+    default:
+      monthlyPrice;
+      break;
+  }
+  if (rangeValue >= 1000) {
+    prefix = "M";
+    rangeValue = 1;
+  } else {
+    prefix = "K";
+  }
+  viewEl.textContent = `${rangeValue}${prefix}`;
+  mainPrice.textContent = `$${monthlyPrice}`;
+  console.log(e.target.value);
 });
