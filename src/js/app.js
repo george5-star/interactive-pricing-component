d3cd64712508d2f4e2e5459ac3affb34e7425ae7
George Asiedu <georgeasiedu543@gmail.com>
2024-01-12 23:09:58 +0000

    Map pageview to monthly price

--- src/js/app.js
+++ src/js/app.js
@@ -13,35 +13,45 @@ toggler.addEventListener("click", () => {
 rangeInputEl.addEventListener("input", (e) => {
   let prefix;
   let rangeValue = Number(e.target.value);
-  let monthlyPrice = 0;
+  let monthlyPrice = mapNum(
+    rangeValue,
+    rangeInputEl.min,
+    rangeInputEl.max,
+    0,
+    36
+  ).toFixed(2);
 
-  switch (rangeValue) {
-    case 10:
-      monthlyPrice = 8;
-      break;
-    case 50:
-      monthlyPrice = 12;
-      break;
-    case 100:
-      monthlyPrice = 16;
-      break;
-    case 500:
-      monthlyPrice = 24;
-      break;
-    case 1000:
-      monthlyPrice = 36;
-      break;
-    default:
-      monthlyPrice;
-      break;
-  }
+  // switch (rangeValue) {
+  //   case 10:
+  //     monthlyPrice = 8;
+  //     break;
+  //   case 50:
+  //     monthlyPrice = 12;
+  //     break;
+  //   case 100:
+  //     monthlyPrice = 16;
+  //     break;
+  //   case 500:
+  //     monthlyPrice = 24;
+  //     break;
+  //   case 1000:
+  //     monthlyPrice = 36;
+  //     break;
+  //   default:
+  //     monthlyPrice;
+  //     break;
+  // }
   if (rangeValue >= 1000) {
     prefix = "M";
-    rangeValue = 1;
   } else {
     prefix = "K";
   }
+
   viewEl.textContent = `${rangeValue}${prefix}`;
   mainPrice.textContent = `$${monthlyPrice}`;
-  console.log(e.target.value);
+  console.log(monthlyPrice);
 });
+
+function mapNum(value, in_min, in_max, out_min, out_max) {
+  return ((value - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+}
