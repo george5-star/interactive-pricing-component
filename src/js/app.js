16a516799f39bddb5510fdc8b152a3b5e729bbc7
George Asiedu <georgeasiedu543@gmail.com>
2024-01-13 22:59:29 +0000

    Add monthly and yearly toggle prices

--- src/js/app.js
+++ src/js/app.js
@@ -2,54 +2,47 @@ const toggler = document.querySelector(".toggle");
 const toggleIndicator = document.querySelector(".indicator");
 const viewEl = document.querySelector(".price-view");
 
+const priceText = document.querySelector(".accent");
+
 const mainPrice = document.querySelector(".main-price");
 
 const rangeInputEl = document.getElementById("range");
 
+let monthlyPrice = 0;
+let yearlyPrice = 0;
+
 toggler.addEventListener("click", () => {
   toggleIndicator.classList.toggle("clicked");
+  if (toggleIndicator.classList.contains("clicked")) {
+    priceText.textContent = "/year";
+    mainPrice.textContent = yearlyPrice;
+  } else {
+    priceText.textContent = "/month";
+    mainPrice.textContent = monthlyPrice;
+  }
 });
 
 rangeInputEl.addEventListener("input", (e) => {
   let prefix;
   let rangeValue = Number(e.target.value);
-  let monthlyPrice = mapNum(
+  monthlyPrice = mapNum(
     rangeValue,
     rangeInputEl.min,
     rangeInputEl.max,
     0,
     36
   ).toFixed(2);
+  yearlyPrice = (monthlyPrice * 12 * 0.75).toFixed(2);
 
-  // switch (rangeValue) {
-  //   case 10:
-  //     monthlyPrice = 8;
-  //     break;
-  //   case 50:
-  //     monthlyPrice = 12;
-  //     break;
-  //   case 100:
-  //     monthlyPrice = 16;
-  //     break;
-  //   case 500:
-  //     monthlyPrice = 24;
-  //     break;
-  //   case 1000:
-  //     monthlyPrice = 36;
-  //     break;
-  //   default:
-  //     monthlyPrice;
-  //     break;
-  // }
+  mainPrice.textContent = `$${monthlyPrice}`;
+
   if (rangeValue >= 1000) {
     prefix = "M";
+    viewEl.textContent = `1${prefix}`;
   } else {
     prefix = "K";
+    viewEl.textContent = `${rangeValue}${prefix}`;
   }
-
-  viewEl.textContent = `${rangeValue}${prefix}`;
-  mainPrice.textContent = `$${monthlyPrice}`;
-  console.log(monthlyPrice);
 });
 
 function mapNum(value, in_min, in_max, out_min, out_max) {
